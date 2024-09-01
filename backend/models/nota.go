eb202cff28b7868f302947e11ac9046202178983
Otávio Ferreira <ottvferreira@gmail.com>
2024-09-01 20:52:54 -0300

    FEAT: Model Notas

--- backend/models/nota.go
+++ backend/models/nota.go
@@ -1,3 +1,4 @@
+// models/nota.go
 package models
 
 import "gorm.io/gorm"
@@ -6,5 +7,6 @@ type Nota struct {
 	gorm.Model
 	AlunoID     uint    `json:"aluno_id"`
 	AtividadeID uint    `json:"atividade_id"`
+	TurmaID     uint    `json:"turma_id"`
 	Nota        float64 `json:"nota"`
 }
